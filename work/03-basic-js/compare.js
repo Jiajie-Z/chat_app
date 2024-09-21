0143cab4f524966b2424773c0a54836f65333f42
Jiajie Zhang <zhang.jiajie1@northeastern.edu>
2024-09-21 15:52:25 -0700

    completed basic JS

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -3,8 +3,21 @@
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
 function compare( word, guess ) {  // DO NOT MODIFY
+  let sameLetters = 0;
+  let tempWord = word.toLowerCase().split(""); 
+  let tempGuess = guess.toLowerCase().split("");
+  
+  for( let i = 0; i < tempWord.length; i++ ){
+    for ( let j = 0; j < tempGuess.length; j++){
+      if (tempWord[i] === tempGuess[j]){
+        sameLetters++;
+        tempGuess.splice(j,1)
+      }
+    }
+  }
 
-/* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+
+
+  return sameLetters;
 }
